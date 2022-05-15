41bf328f4aa0f7ee9e9ecf09abe31a320f31457b
khalilnazari <khalil.deve@gmail.com>
2022-05-15 13:51:51 +0800

    Containers are added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import {Article} from './components'
+import './app.css'
+
+import { Header, Footer, Blog, Posibilities, Feature, WhatGPT3 } from './containers'
+import { CTA, Brand, Navbar } from './components'
 
 const App = () => {
   return (
-    <div>
-      <Article />
+    <div className='App'>
+      <div className='gradiant__bg'>
+        <Navbar />
+        <Header />
+      </div>
+
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Posibilities />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
---
+++ src/containers/posibilities/Posibilities.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './posibilities.css'
+
+const Posibilities = () => {
+  return (
+    <div>Posibilities</div>
+  )
+}
+
+export default Posibilities
